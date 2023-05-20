f151b133a1bf741de6ed4697e4dd6c2d70532793
Priyanshu tandekar <priyanshutandekar2001@gamil.com>
2023-05-20 13:25:27 +0530

    pp

---
+++ src/components/Course_card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Course_card() {
+  return (
+    <>
+     <p className='text-center'>Our Courses</p>
+     <h1 className='text-4xl'>Explore Our Popular Online Courses</h1>
+     {/* hhjhjhjh */}
+     {/* sfsfsffsfsf */}
+    </>
+  )
+}
